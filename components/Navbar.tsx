64a5f7df5c735c7212c7c7eeecf8d9a434930053
Дмитрий Дьячков (Dmitri Diakov) <dmitridiakov@gmail.com>
2023-05-23 13:13:16 +0300

    closed navbar routes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -41,7 +41,7 @@ const Navbar = () => {
     <nav className="w-full fixed z-40">
       <div className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}`}>
         <img src="/images/logo.png" className="h-4 lg:h-7" alt="Logo" />
-        <div className="flex-row ml-8 gap-7 hidden lg:flex">
+        <div className="flex-row ml-8 gap-7 hidden lg:flex line-through">
           <NavbarItem label="Home" active />
           <NavbarItem label="Series" />
           <NavbarItem label="Films" />
@@ -49,7 +49,7 @@ const Navbar = () => {
           <NavbarItem label="My List" />
           <NavbarItem label="Browse by Languages" />
         </div>
-        <div onClick={toggleMobileMenu} className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
+        <div onClick={toggleMobileMenu} className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative line-through">
           <p className="text-white text-sm">Browse</p>
           <ChevronDownIcon className={`w-4 text-white fill-white transition ${showMobileMenu ? 'rotate-180' : 'rotate-0'}`} />
           <MobileMenu visible={showMobileMenu} />
